2a8b05d203079ed635157f2409a5bd723be48066
Hossain Khademian <hossain.khademian@gmail.com>
2022-01-09 07:55:00 +0330

    use Kinv method and add test function

--- cordic-sim.cpp
+++ cordic-sim.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 
-/// to use Fixed<m,n> use zero or comment it
+/// to use Fixed<m,n> use false or 0 or comment it
 #define CORDIC_FLOAT false
 
 #include "cordic.hpp"
@@ -19,41 +19,49 @@ using CordicT = Cordic<InnerT>;
 
 using namespace std;
 
-CordicT run(int steps, const InnerT &tet) {
-    CordicT cordic{.n=0, .x=1, .y=0, .z=tet};
+CordicT run(int steps, bool log, const InnerT &tet) {
+    CordicT cordic{.n=0, .x=CordicT::Kinv, .y=0, .z=tet};
 
-    cout << cordic << endl;
-    for (auto i = 0; (steps <= 0 && cordic.n < 32 && abs((long double) cordic.z) > 0.00001) || i < steps; ++i) {
+    if (log) cout << cordic << endl;
+    for (auto i = 0; (steps <= 0 && cordic.n < 32 && abs((long double) cordic.z) > 0.0001) || i < steps; ++i) {
         cordic = cordic.step<CordicT::TYP_CIRCULAR, CordicT::MOD_ROTATION>();
-        cout << cordic << endl;
+        if (log) cout << cordic << endl;
     }
 
     return cordic;
 }
 
-void log(const CordicT &cordic, const InnerT &tet) {
+void print(const CordicT &cordic, const InnerT &tet) {
+    const auto realC = cos((long double) tet);
+    const auto realS = sin((long double) tet);
+    const auto cordicC = ((long double) cordic.x);
+    const auto cordicS = ((long double) cordic.y);
     cout << "tet: " << ((long double) tet) << " deg: " << ((long double) tet) * 180 / M_PI << endl;
-    cout << "real   cos: " << cos((long double) tet) << endl;
-    cout << "cordic cos: " << ((long double) cordic.x) / CordicT::K << endl;
-    cout << "real   sin: " << sin((long double) tet) << endl;
-    cout << "cordic sin: " << ((long double) cordic.y) / CordicT::K << endl;
+    // cout << "real   cos: " << realC << endl;
+    // cout << "cordic cos: " << cordicC << endl;
+    cout << "acc cos: " << 100 - abs(realC - cordicC) * 100 << endl;
+    // cout << "real   sin: " << realS << endl;
+    // cout << "cordic sin: " << cordicS << endl;
+    cout << "acc sin: " << 100 - abs(realS - cordicS) * 100 << endl;
     cout << endl;
+}
+
+void test(int step, bool log, const InnerT &tet) {
+    auto cordic = run(step, log, tet);
+    print(cordic, tet);
 }
 
 int main() {
     /* to find how many steps is needed to reach
      * required precision */
-    log(run(0, M_PI / 6), M_PI / 6);
-    exit(0);
+//    test(0, true, M_PI / 6), exit(0);
 
     const InnerT tets[] = {
-            0, M_PI / 12, M_PI / 6, M_PI / 4,
-            M_PI / 2, 3 * M_PI / 4, M_PI,
+            0, M_PI / 12, M_PI / 6, M_PI / 4, M_PI / 2,
     };
 
     for (const auto &tet : tets) {
-        auto cordic = run(14, tet);
-        log(cordic, tet);
+        test(13, false, tet);
     }
     return 0;
 }
